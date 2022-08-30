76c85f814333c83eab8f726417c67483cd98d7a7
v-nebe <katya.shauneva@gmail.com>
2022-08-30 09:30:15 +0300

    tasks have been completed

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,7 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public void roundNumber(float numberToBeRounded){
+        System.out.println(Math.round(numberToBeRounded));
     }
 }
