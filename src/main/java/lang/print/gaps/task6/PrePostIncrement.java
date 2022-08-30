262774251f091631cd62c5ab1f7b0a785d43875e
v-nebe <katya.shauneva@gmail.com>
2022-08-30 09:33:19 +0300

    tasks have been completed

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -11,7 +11,7 @@ public class PrePostIncrement {
         int x = ++a   + b +   c   + d;
         System.out.println("Expected x is 40, x = " + x);
         // print the updated value of a, b, c, and d  
-        System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
+        System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + ++c + " and d = " + d);
 
     }
 }
